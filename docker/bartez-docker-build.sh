91f417ed2c5c53ef00562000e8572eca72070311
crsnplusplus@gmail.com <crsnplusplus@gmail.com>
2018-02-08 10:49:04 +0100

    added --rm to docker run command

--- docker/bartez-docker-build.sh
+++ docker/bartez-docker-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t crsnplusplus/bartez-docker-dev .
+docker build --no-cache -t crsnplusplus/bartez-docker-dev .
--- docker/bartez-docker-start.sh
+++ docker/bartez-docker-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -i -p 3500:3500 crsnplusplus/bartez-docker-dev python /home/projects/bartez/bartez_remote_dbg.py
+docker run -i -p 3500:3500 --rm crsnplusplus/bartez-docker-dev python /home/projects/bartez/bartez_remote_dbg.py
