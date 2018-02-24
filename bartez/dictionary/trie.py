24c080309aa4955027f8b8716a275439e4009e16
crsnplusplus@gmail.com <crsnplusplus@gmail.com>
2018-02-24 23:16:33 +0100

    added trie (prefix tree) visitors, and tests

---
+++ bartez/dictionary/trie.py
@@ -0,0 +1,68 @@
+from bartez.dictionary.trie_node import BartezNode, BartezNodeTerminal, BartezNodeNonTerminal
+
+
+class BartezTrie(object):
+    """Bartez trie, used by dictionary"""
+
+    def __init__(self, language, file):
+        self.__language = language
+        self.__file = file
+        self.__root = None
+        self.__load()
+
+
+    def get_language(self):
+        return self.__language
+
+
+    def get_file(self):
+        return self.__file
+
+
+    def get_root(self):
+        return self.__root
+
+
+    def is_loaded(self):
+        return self.__root is not None
+
+
+    def add_word(self, word):
+        if self.is_loaded() is False:
+            return
+
+        parent = self.__root
+        child = None
+        for pos in range(len(word)):
+            current_char = word[pos]
+            child = parent.get_child(current_char)
+            if child == None:
+                child = BartezNodeNonTerminal(parent, current_char)
+                parent.add_child(child)
+
+            parent = child
+
+        if parent.has_terminal() == False:
+            child = BartezNodeTerminal(parent)
+            parent.add_child(child)
+
+    def __load(self):
+        if self.__root == None:
+            self.__root = BartezNodeNonTerminal(None, '')
+
+        first = '0'
+
+        with open(self.__file) as f:
+            for word in f:
+                word = word.replace("\n", "")
+                word = word.replace("\r", "")
+
+                if len(word) < 2:
+                    continue
+
+                if first != word[0]:
+                    first = word[0]
+                    print("adding page: " + str(first))
+                    
+                
+                self.add_word(word.upper())
---
+++ bartez/tests/test_trie.py
@@ -0,0 +1,39 @@
+import unittest
+from bartez.dictionary.trie import *
+from bartez.dictionary.trie_node_visitor import *
+
+class Test_bartez_trie(unittest.TestCase):
+    def test_bartez_trie_creation(self):
+        trie = BartezTrie('italian', 'words_test_1000.txt')
+        self.assertTrue(trie.is_loaded() == True)
+
+
+    #def test_bartez_trie_visitor_print(self):
+    #    trie = BartezTrie('italian', 'words_test_1000.txt')
+    #    printVisitor = BartezNodeVisitorPrint()
+    #    root = trie.get_root()
+    #    root.accept(printVisitor)
+    #    self.assertTrue(trie.is_loaded() == True)
+
+
+    def test_bartez_trie_visitor_list(self):
+        trie = BartezTrie('italian', 'words_test_1000.txt')
+        listVisitor = BartezNodeVisitorList()
+        root = trie.get_root()
+        root.accept(listVisitor)
+        words = listVisitor.get_words()
+        len_words = len(words)
+        self.assertTrue(len(words) == 1000)
+
+
+    def test_bartez_trie_visitor_list_long(self):
+        trie = BartezTrie('italian', 'words.txt')
+        listVisitor = BartezNodeVisitorList()
+        root = trie.get_root()
+        root.accept(listVisitor)
+        words = listVisitor.get_words()
+        len_words = len(words)
+        self.assertTrue(len(words) == 281614)
+
+if __name__ == '__main__':
+    unittest.main()
