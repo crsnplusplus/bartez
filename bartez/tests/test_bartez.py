983a68fa81457720cbd3d6c83544eb8a4845cf5c
crsnplusplus@gmail.com <crsnplusplus@gmail.com>
2018-02-27 22:46:49 +0100

    removed duplicated function

--- bartez/tests/test_bartez.py
+++ bartez/tests/test_bartez.py
@@ -10,11 +10,6 @@ class TestBartez(unittest.TestCase):
         self.assertTrue(dictionary.get_words_count() > 0)
         self.assertTrue(len(dictionary.get_language()) > 0)
 
-    def test_bartez_dictionary(self):
-        dictionary = get_test_dictionary_1000()
-        self.assertTrue(dictionary.get_page_count() > 0)
-        self.assertTrue(dictionary.get_words_count() > 0)
-        self.assertTrue(len(dictionary.get_language()) > 0)
 
 if __name__ == '__main__':
     unittest.main()
